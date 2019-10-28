09aa795fe6bc576cba45bcb14edec29926e6cb2d
katy-more-rsi <katy.more@ruralsourcing.com>
2019-10-28 11:04:44 -0500

    added some comments to improve readability of code

--- src/main/java/com/ruralsourcing/WeatherBoy/model/DblObservation.java
+++ src/main/java/com/ruralsourcing/WeatherBoy/model/DblObservation.java
@@ -2,6 +2,9 @@ package com.ruralsourcing.WeatherBoy.model;
 
 import com.google.gson.annotations.SerializedName;
 
+
+//DblObservation is the class model to hold the observations
+//where the value is of type double
 public class DblObservation {
 
     @SerializedName("date_time")
--- src/main/java/com/ruralsourcing/WeatherBoy/model/StrObservation.java
+++ src/main/java/com/ruralsourcing/WeatherBoy/model/StrObservation.java
@@ -2,6 +2,9 @@ package com.ruralsourcing.WeatherBoy.model;
 
 import com.google.gson.annotations.SerializedName;
 
+
+//StrObservation is the class model to hold the observations
+//where the value is of type String
 public class StrObservation {
     @SerializedName("date_time")
     private String dateTime;
